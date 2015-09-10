df0bd46422d01896c0fbc159bd7df3e6b6567152
Hari Jiang <hari.jiang@outlook.com>
2015-09-10 21:25:06 +0800

    add context.Render method

--- context.go
+++ context.go
@@ -1,6 +1,7 @@
 package ringo
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 )
@@ -13,4 +14,20 @@ type Context struct {
 
 func NewContext() *Context {
 	return &Context{}
+}
+
+func (c *Context) Render(statusCode int, content interface{}) {
+	w := c.ResponseWriter.(*ResponseWriter)
+	var byteContent []byte
+
+	switch content.(type) {
+	case []byte:
+		byteContent = content.([]byte)
+	case string:
+		byteContent = []byte(content.(string))
+	default:
+		byteContent = []byte(fmt.Sprint(content))
+	}
+	w.WriteHeader(statusCode)
+	w.Write(byteContent)
 }
