cfbf7f618bd378fd0e0cbd8257a32af46a14e396
Hari Jiang <hari.jiang@outlook.com>
2015-09-12 13:32:50 +0800

    add recover middleware

---
+++ middleware/recover.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/jjyr/ringo"
+)
+
+func Recover() ringo.MiddlewareFunc {
+	return func(handler ringo.HandlerFunc) ringo.HandlerFunc {
+		return func(c *ringo.Context) {
+			defer func() {
+				if r := recover(); r != nil {
+					log.Printf("error: %s", r)
+					if !c.Rendered() {
+						statusCode := http.StatusInternalServerError
+						text := http.StatusText(statusCode)
+						c.Render(statusCode, text)
+					}
+				}
+			}()
+			handler(c)
+		}
+	}
+}
