19ad9051bf595b2784b1b0162b8c6adb04d8d9d7
Hari Jiang <hari.jiang@outlook.com>
2015-09-12 13:33:05 +0800

    update sample main.go

--- bin/main.go
+++ bin/main.go
@@ -5,21 +5,24 @@ import (
 	"log"
 
 	"github.com/jjyr/ringo"
+	"github.com/jjyr/ringo/middleware"
 )
 
 func main() {
 	flag.Parse()
 	app := ringo.NewApp()
-	r := app.GetRouter()
-	r.GET("/ping", func(c *ringo.Context) {
+	// r := app.GetRouter()
+	app.GET("/ping", func(c *ringo.Context) {
 		log.Print("pong!")
 		c.Render(200, "pong!")
 	})
 
-	r.GET("/numbers/:n/echo", func(c *ringo.Context) {
+	app.GET("/numbers/:n/echo", func(c *ringo.Context) {
 		log.Print(c.URL.Query())
 		log.Print(c.Params.Get("n"))
 	})
+
+	app.Use(middleware.Recover())
 
 	app.Run("localhost:8020")
 }
