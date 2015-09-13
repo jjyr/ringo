e2dc08027f74727384b97de87ced977d52d477d9
Hari Jiang <hari.jiang@outlook.com>
2015-09-13 15:52:49 +0800

    Support options & any, fix match bug

--- router.go
+++ router.go
@@ -22,11 +22,15 @@ func (r *Router) DELETE(path string, handler HandlerFunc) {
 func (r *Router) HEAD(path string, handler HandlerFunc) {
 	r.AddRoute(path, "HEAD", handler)
 }
-func (r *Router) OPTION(path string, handler HandlerFunc) {
-	r.AddRoute(path, "OPTION", handler)
+func (r *Router) OPTIONS(path string, handler HandlerFunc) {
+	r.AddRoute(path, "OPTIONS", handler)
 }
 func (r *Router) PATCH(path string, handler HandlerFunc) {
 	r.AddRoute(path, "PATCH", handler)
+}
+
+func (r *Router) Any(path string, handler HandlerFunc) {
+	r.AddRoute(path, "*", handler)
 }
 
 func (router *Router) Mount(path string, mountedRouter *Router) {
@@ -45,13 +49,14 @@ type routeHandler struct {
 }
 
 type Router struct {
-	routes []routeHandler
+	routes    []routeHandler
+	anyRoutes []routeHandler
 }
 
 var pathParamRegexp *regexp.Regexp
 
 func init() {
-	pathParamRegexp = regexp.MustCompile("\\:(\\w+)([/$])")
+	pathParamRegexp = regexp.MustCompile("\\:(\\w+)(/|\\z)")
 }
 
 func compilePathExp(path string) (*regexp.Regexp, error) {
@@ -59,16 +64,22 @@ func compilePathExp(path string) (*regexp.Regexp, error) {
 	log.Printf("escaped route path: %s", escaped)
 	replaced := pathParamRegexp.ReplaceAllString(escaped, "(?P<$1>\\w+)$2")
 	log.Printf("replaced route path: %s", replaced)
-	return regexp.Compile(fmt.Sprintf("^%s$", replaced))
+	return regexp.Compile(fmt.Sprintf("\\A%s\\z", replaced))
 }
 
 func (r *Router) AddRoute(path string, method string, handler HandlerFunc) {
-	// key := fmt.Sprint("%s/%s", method, path)
 	pathRegexp, err := compilePathExp(path)
 	if err != nil {
 		log.Fatalf("Path compile error, please check syntax:\n[%s]%s\n%s", method, path, err)
 	}
-	r.routes = append(r.routes, routeHandler{path: path, method: method, handler: handler, pathRegexp: pathRegexp})
+
+	route := routeHandler{path: path, method: method, handler: handler, pathRegexp: pathRegexp}
+
+	if method == "*" {
+		r.anyRoutes = append(r.anyRoutes, route)
+	} else {
+		r.routes = append(r.routes, route)
+	}
 	// if _, ok = r.routes[key]; ok {
 	// 	// log.Fatalf.Fatalf("[%s]%s already exists, duplicate route!", method, path)
 	// } else {
@@ -77,7 +88,15 @@ func (r *Router) AddRoute(path string, method string, handler HandlerFunc) {
 }
 
 func (r *Router) MatchRoute(path string, method string) (HandlerFunc, *url.Values) {
-	for _, route := range r.routes {
+	handler, params := matchRouteHandler(path, method, r.routes)
+	if handler != nil {
+		return handler, params
+	}
+	return matchRouteHandler(path, "*", r.anyRoutes)
+}
+
+func matchRouteHandler(path string, method string, routes []routeHandler) (HandlerFunc, *url.Values) {
+	for _, route := range routes {
 		log.Printf("try match [%s]%s by route %+v", method, path, route)
 		if route.method == method {
 			subMatch := route.pathRegexp.FindAllStringSubmatch(path, -1)
@@ -85,7 +104,7 @@ func (r *Router) MatchRoute(path string, method string) (HandlerFunc, *url.Value
 				subMatchNames := route.pathRegexp.SubexpNames()[1:]
 				pathParams := url.Values{}
 				for i, matchName := range subMatchNames {
-					pathParams.Set(matchName, subMatch[i][1])
+					pathParams.Set(matchName, subMatch[0][i+1])
 				}
 
 				return route.handler, &pathParams
