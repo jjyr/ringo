6269be28991f7c5c8ae5ef41d643aa778ddf3a86
Hari Jiang <hari.jiang@outlook.com>
2015-09-28 17:22:51 +0800

    support auto detect controller name

--- controller.go
+++ controller.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"path"
 	"reflect"
+	"strings"
 )
 
 type Controllerable interface {
@@ -12,6 +13,43 @@ type Controllerable interface {
 
 type Controller struct {
 	Name string
+}
+
+func isAlphabetUpper(s string) bool {
+	return strings.ToUpper(s) == s
+}
+
+func getControllerName(c Controllerable) string {
+	name := c.ControllerName()
+
+	if name == "" {
+		controllerType := fmt.Sprintf("%T", c)
+		// *pkg.TestRingoController
+		tmpName := controllerType
+		dotIndex := strings.LastIndex(tmpName, ".")
+		if dotIndex > -1 && len(tmpName) > dotIndex {
+			// TestRingoController
+			tmpName = tmpName[dotIndex+1:]
+			if idx := strings.Index(tmpName, "Controller"); idx > 0 {
+				// TestRingo
+				tmpName = tmpName[0:idx]
+
+				prev := ""
+				for _, w := range tmpName {
+					s := string(w)
+					if isAlphabetUpper(s) {
+						if !isAlphabetUpper(prev) {
+							s = "-" + s
+						}
+					}
+					// test-ringo
+					name += strings.ToLower(s)
+				}
+			}
+		}
+	}
+
+	return name
 }
 
 func (c *Controller) ControllerName() string {
@@ -37,7 +75,7 @@ func init() {
 }
 
 func registerToRouter(r *Router, c Controllerable) {
-	controllerName := c.ControllerName()
+	controllerName := getControllerName(c)
 	if controllerName == "" {
 		panic(fmt.Errorf("Controller Name is empty, %+v", c))
 	}
