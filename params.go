35a758b2639cadb5fc0dae166ee848037f188f26
Hari Jiang <hari.jiang@outlook.com>
2015-10-01 13:47:16 +0800

    add byName func

--- params.go
+++ params.go
@@ -3,3 +3,7 @@ package ringo
 import "github.com/julienschmidt/httprouter"
 
 type Params httprouter.Params
+
+func (params *Params) ByName(name string) string {
+	return httprouter.Params(*params).ByName(name)
+}
